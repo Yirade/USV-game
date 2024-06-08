65df6ad13e980c073f6ba6f1f0a05039d44b1f2f
Nicolae Tugui <NicusorN5@proton.me>
2024-06-08 03:04:44 +0300

    Pathfinding tiles

--- USVGame/USVGame/Content/Screens/LvlC0/Interiour.tsx
+++ USVGame/USVGame/Content/Screens/LvlC0/Interiour.tsx
@@ -1,6 +1,9 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.2" name="Interiour" tilewidth="16" tileheight="16" tilecount="34048" columns="32">
- <image source="Interiour.png" width="512" height="17024"/>
+<tileset version="1.10" tiledversion="1.10.2" name="Interiour" tilewidth="16" tileheight="16" tilecount="30432" columns="32">
+ <image source="Interiour.png" width="512" height="15216"/>
+ <tile id="156" type="WalkableTile"/>
+ <tile id="157" type="WalkableTile"/>
+ <tile id="158" type="WalkableTile"/>
  <tile id="160">
   <objectgroup draworder="index" id="2">
    <object id="1" x="3" y="12" width="13" height="4"/>
@@ -16,6 +19,9 @@
    <object id="1" x="0" y="12" width="13" height="4"/>
   </objectgroup>
  </tile>
+ <tile id="188" type="WalkableTile"/>
+ <tile id="189" type="WalkableTile"/>
+ <tile id="190" type="WalkableTile"/>
  <tile id="192">
   <objectgroup draworder="index" id="2">
    <object id="1" x="3" y="0" width="13" height="16"/>
@@ -61,6 +67,12 @@
    <object id="1" x="0" y="0" width="13" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="1712" type="WalkableTile"/>
+ <tile id="1713" type="WalkableTile"/>
+ <tile id="1744" type="WalkableTile"/>
+ <tile id="1745" type="WalkableTile"/>
+ <tile id="1776" type="WalkableTile"/>
+ <tile id="1777" type="WalkableTile"/>
  <tile id="1908">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="0" width="16" height="16"/>
@@ -137,9 +149,12 @@
    <object id="1" x="0" y="0" width="16" height="16"/>
   </objectgroup>
  </tile>
+ <tile id="30029" type="WalkableTile"/>
  <tile id="30378">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="-0.0434783" width="5.08696" height="16.0435"/>
   </objectgroup>
  </tile>
+ <tile id="30379" type="WalkableTile"/>
+ <tile id="30380" type="WalkableTile"/>
 </tileset>
